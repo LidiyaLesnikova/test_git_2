695bcdbef94425363ac1e2db414a736b2a027b2a
Lidiya <lesnikoval@yandex.ru>
2023-05-30 23:23:18 +0300

    2 commit

--- test.java
+++ test.java
@@ -2,7 +2,9 @@ package Test_git_2;
 
 public class test {
     public static void main(String[] args) {
-        
+        System.out.println("Первая строка");
+        System.out.println("Вторая строка");
+        System.out.println("Третья строка");
     }
     
 }
